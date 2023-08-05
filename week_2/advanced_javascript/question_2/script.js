2fa9e94b82f2fb03cd15ddd424832736949ba3bb
Rohit <rohitdeshpande9922@gmail.com>
2023-08-05 19:30:26 +0530

    Question_2 completed

---
+++ week_2/advanced_javascript/question_2/script.js
@@ -0,0 +1,27 @@
+const accordionItems = document.querySelectorAll(".accordion__item");
+
+// console.log(accordionItems[0].children[0]);
+
+accordionItems.forEach((item) =>{
+    item.addEventListener("click",() =>{
+        const question = item.children[0];
+        const answer = item.children[1];
+
+        const span = question.children[0].children[1].children[0];
+        if(answer.classList.contains("accordion__answer__inactive"))
+        {
+            answer.classList.remove("accordion__answer__inactive");
+            answer.classList.add("accordion__answer__active");
+            span.classList.remove("fa-chevron-down");
+            span.classList.add("fa-chevron-up");
+        }else{
+            answer.classList.remove("accordion__answer__active");
+            answer.classList.add("accordion__answer__inactive");
+            span.classList.remove("fa-chevron-up");
+            span.classList.add("fa-chevron-down");
+        }
+
+    })
+})
+
+
