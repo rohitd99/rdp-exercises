4ce9592c58d9c604f7924e8f566eac27909bc597
Rohit <rohitdeshpande9922@gmail.com>
2023-08-24 12:57:00 +0530

    Review Change: Used .toggle instead of .add & .remove

--- week_2/advanced_javascript/question_2/script.js
+++ week_2/advanced_javascript/question_2/script.js
@@ -5,22 +5,12 @@ accordionItems.forEach((item) =>{
     item.addEventListener("click",() =>{
         const question = item.children[0];
         const answer = item.children[1];
-
         const span = question.children[0].children[1].children[0];
-        if(answer.classList.contains("accordion__answer__inactive"))
-        {
-            answer.classList.remove("accordion__answer__inactive");
-            answer.classList.add("accordion__answer__active");
-            span.classList.remove("fa-chevron-down");
-            span.classList.add("fa-chevron-up");
-        }else{
-            answer.classList.remove("accordion__answer__active");
-            answer.classList.add("accordion__answer__inactive");
-            span.classList.remove("fa-chevron-up");
-            span.classList.add("fa-chevron-down");
-        }
 
+        answer.classList.toggle("accordion__answer__inactive");
+        answer.classList.toggle("accordion__answer__active");
+        span.classList.toggle("fa-chevron-up");
+        span.classList.toggle("fa-chevron-down");
     })
 })
-
 
