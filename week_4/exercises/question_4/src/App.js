c8c4820b85e1b8adc2c82099813ad246e571e90f
Rohit <rohitdeshpande9922@gmail.com>
2023-08-24 12:02:55 +0530

    Review change: .map for Cards

--- week_4/exercises/question_4/src/App.js
+++ week_4/exercises/question_4/src/App.js
@@ -2,46 +2,69 @@ import './App.css';
 import Card from './components/card';
 
 function App() {
+
+
+    const Cards = [
+        {
+            title      : "The evolution of design thinking: insights and perspectives",
+            imgSrc     : "./img/image1.jpg", 
+            imgAlt     : "Image of sunset",
+            category   : "Thoughts",
+            timeToRead : "4min"
+        },
+        {
+            title      : "The evolution of design thinking: insights and perspectives",
+            imgSrc     : "./img/image1.jpg", 
+            imgAlt     : "Image of sunset",
+            category   : "Thoughts",
+            timeToRead : "4min"
+        },
+        {
+            title      : "The evolution of design thinking: insights and perspectives",
+            imgSrc     : "./img/image1.jpg", 
+            imgAlt     : "Image of sunset",
+            category   : "Thoughts",
+            timeToRead : "4min"
+        },
+        {
+            title      : "The evolution of design thinking: insights and perspectives",
+            imgSrc     : "./img/image1.jpg", 
+            imgAlt     : "Image of sunset",
+            category   : "Thoughts",
+            timeToRead : "4min"
+        },
+        {
+            title      : "The evolution of design thinking: insights and perspectives",
+            imgSrc     : "./img/image1.jpg", 
+            imgAlt     : "Image of sunset",
+            category   : "Thoughts",
+            timeToRead : "4min"
+        },
+        {
+            title      : "The evolution of design thinking: insights and perspectives",
+            imgSrc     : "./img/image1.jpg", 
+            imgAlt     : "Image of sunset",
+            category   : "Thoughts",
+            timeToRead : "4min"
+        }
+    ];
+        
     return (
         <div className="App">
             <main>
                 <section className="cards">
-                    <Card   title="The evolution of design thinking: insights and perspectives" 
-                            imgSrc="./img/image1.jpg" 
-                            imgAlt="Image of sunset"
-                            category="Thoughts"
-                            timeToRead="4min"
-                    ></Card>
-                    <Card   title="The evolution of design thinking: insights and perspectives" 
-                            imgSrc="./img/image1.jpg" 
-                            imgAlt="Image of sunset"
-                            category="Thoughts"
-                            timeToRead="4min"
-                    ></Card>
-                    <Card   title="The evolution of design thinking: insights and perspectives" 
-                            imgSrc="./img/image1.jpg" 
-                            imgAlt="Image of sunset"
-                            category="Thoughts"
-                            timeToRead="4min"
-                    ></Card>
-                    <Card   title="The evolution of design thinking: insights and perspectives" 
-                            imgSrc="./img/image1.jpg" 
-                            imgAlt="Image of sunset"
-                            category="Thoughts"
-                            timeToRead="4min"
-                    ></Card>
-                    <Card   title="The evolution of design thinking: insights and perspectives" 
-                            imgSrc="./img/image1.jpg" 
-                            imgAlt="Image of sunset"
-                            category="Thoughts"
-                            timeToRead="4min"
-                    ></Card>
-                    <Card   title="The evolution of design thinking: insights and perspectives" 
-                            imgSrc="./img/image1.jpg" 
-                            imgAlt="Image of sunset"
-                            category="Thoughts"
-                            timeToRead="4min"
-                    ></Card>
+                    {
+                        Cards.map((card) => {
+                            return(
+                                <Card   title={card.title} 
+                                        imgSrc={card.imgSrc} 
+                                        imgAlt={card.imgAlt}
+                                        category={card.category}
+                                        timeToRead={card.timeToRead}
+                                ></Card>
+                            );
+                        })
+                    }
                 </section>
             </main>
         </div>
